0d32e65a2ec421f6bb7e2fb13fd05c0195f01c3c
Nithin Joseph <njoseph@terpmail.umd.edu>
2023-02-23 10:59:41 -0500

    access convert_lyrics from class

--- MediaService.py
+++ MediaService.py
@@ -71,6 +71,24 @@ class MediaService:
 
         return 'separated\\htdemucs_ft'+path.replace(self.path, '')[:-4]+'/'
 
+    def convert_lyrics(self, segments, artist, song):
+        openai_output = segments
+        lrc_output = ""
+        for segment in openai_output:
+            start_time = int(segment["start"])
+            end_time = int(segment["end"])
+            text = segment["text"]
+            lrc_output += f"[{start_time:02}:{int((start_time % 1) * 100):02}]"
+            lrc_output += f"[{end_time:02}:{int((end_time % 1) * 100):02}]"
+            lrc_output += text + "\n"
+        
+        with open('lrc/'+artist+' - '+song+'.lrc', 'w', encoding='utf-8') as f:
+            f.write(lrc_output)
+        return jsonify({
+            'lrc_output': lrc_output,
+            'saved_path': 'lrc/'+artist+' - '+song+'.lrc'
+        })
+
 class MyLogger(object):
     def debug(self, msg):
         pass
